49adea80a0aa67a40afcb6940528f571326e2079
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-01-21 21:30:18 +0200

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [  personsState, setPersons ] = useState({
     persons: [
       { name: 'Vlad', age: 24 },
       { name: 'Olga', age: 25 },
       { name: 'Vira', age: 30 }
     ],
     otherState: 'some other value'
-  };
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersons({
       persons: [
         { name: 'Vladyslav', age: 24 },
         { name: 'Olga', age: 10 },
         { name: 'Viralia', age: 30 }
       ]
     });
-    console.log(this.state.otherState);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm react app</h1>
-        <p>THis is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'm react app</h1>
+      <p>THis is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
-export default App;
+export default app;
