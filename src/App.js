79769023f11e4de79b68451f5b0141f9b5ca42ea
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-01-22 22:31:15 +0200

    Revert "Practice - The Base Syntax"
    This reverts commit cfd19d5777c0a98121a97a5e0f14aa57bdf89e81.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    users: [
-      'Vlad',
-      'Olga',
-      'Vira'
-    ]
+    persons: [
+      { name: 'Vlad', age: 24 },
+      { name: 'Olga', age: 25 },
+      { name: 'Vira', age: 30 }
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 24 },
+        { name: 'Olga', age: 10 },
+        { name: 'Viralia', age: 30 }
+      ]
+    });
+    console.log(this.state.otherState);
   };
 
   nameChangeHandler = (event) => {
     this.setState({
-      users: [
-        'Vlad',
-        event.target.value,
-        'Viralia'
+      persons: [
+        { name: 'Vlad', age: 24 },
+        { name: event.target.value, age: 25 },
+        { name: 'Viralia', age: 30 }
       ]
     });
   };
 
   render() {
+    const style= {
+      backgroundColor: 'white',
+      font: 'ingerit',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-        <UserInput
-          name={this.state.users[1]}
-          changed={this.nameChangeHandler} />
-        <UserOutput
-          name={this.state.users[0]}>My Hobbies: Racing</UserOutput>
-        <UserOutput
-          name={this.state.users[1]}>My Hobbies: Diving</UserOutput>
-        <UserOutput
-          name={this.state.users[3]}>My Hobbies: Laying</UserOutput>
+        <h1>Hi, I'm react app</h1>
+        <p>THis is really working!</p>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Vld123!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Vld!!')}
+          changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
