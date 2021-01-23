773aca6d06d272b8199ba9b6451770f373a096d6
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-01-24 00:16:50 +0200

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -19,14 +19,16 @@ class App extends Component {
     this.setState({persons: persons});
   };
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Vlad', age: 24 },
-        { name: event.target.value, age: 25 },
-        { name: 'Viralia', age: 30 }
-      ]
-    });
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   };
 
   togglePersonsHandler = () => {
@@ -52,7 +54,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
           })}
         </div>
       );
