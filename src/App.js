27dd02f87d05d9db816d00173e21da9c1ab5e82b
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-01-24 00:28:22 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,76 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: 'n1', name: 'Vlad', age: 24 },
-      { id: 'n2', name: 'Olga', age: 25 },
-      { id: 'n3', name: 'Vira', age: 30 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
-  };
+  state = {};
 
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  };
-
-  nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => p.id === id);
-    const person = {...this.state.persons[personIndex]};
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({persons: persons});
-  };
-
-  togglePersonsHandler = () => {
-    this.setState({ showPersons: !this.state.showPersons });
-  };
-
-  render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'ingerit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div >
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)} />
-          })}
-        </div>
-      );
-    }
-
-    return (
-      <div className="App">
-        <h1>Hi, I'm react app</h1>
-        <p>THis is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
-    );
-  }
+  render() { return null}
 }
 export default App;
