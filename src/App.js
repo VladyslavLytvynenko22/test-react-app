8cca7a26f120dc301d31e4ac06d478d90bc3e922
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-01-24 02:37:31 +0200

    Lists & Conditionals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
+import ValidatorComponent from './ValidatorComponent/ValidatorComponent';
+import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component {
-  state = {};
+  state = {
+    text: '',
+    length: 0,
+    textArr: []
+  };
 
-  render() { return null}
+  textLengthHandler = (event) => {
+    const text = event.target.value;
+    const length = event.target.value.length;
+    const textArr = event.target.value.split('').map((char, index) => {
+      return {
+        id: index,
+        char: char
+      };
+    });
+
+    this.setState({
+      text: text,
+      length: length,
+      textArr: textArr
+    });
+  };
+
+  deleteCharHandler = (index) => {
+    const textArr = [...this.state.textArr];
+    textArr.splice(index, 1);
+
+    const text = textArr.map(symbol => symbol.char).join('');
+
+    this.setState({
+      text: text,
+      length: text.length,
+      textArr: textArr
+    });
+  };
+
+  render() {
+    const length = this.state.length;
+    const textArr = [...this.state.textArr];
+    const text = this.state.text;
+    const chars = textArr.length > 0 ?
+      textArr.map((symbol, index) =>
+        <CharComponent
+          letter={symbol.char}
+          click={() => this.deleteCharHandler(index)}
+          key={symbol.id} />) : null;
+
+    return (
+      <div className="App">
+        <input type="text" onChange={this.textLengthHandler} value={text} />
+        <p>Length: {length}</p>
+        <ValidatorComponent length={length} />
+        {chars}
+      </div>
+    );
+  };
 }
+
 export default App;
